423dfd286364e9f9dcb2e93b4bdda36eac879e02
hp <hp@DESKTOP-SNCN38L>
2022-03-24 14:33:33 +0530

    Enam-Vtiger-Project

---
+++ com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateContact.java
@@ -0,0 +1,9 @@
+package com.crm.createOrg;
+
+public class CreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("Create contact2");
+	}
+
+}
---
+++ com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateOrg1.java
@@ -0,0 +1,9 @@
+package com.crm.createOrg;
+
+public class CreateOrg1 {
+
+	public static void main(String[] args) {
+		System.out.println("Create Organization");
+	}
+
+}
