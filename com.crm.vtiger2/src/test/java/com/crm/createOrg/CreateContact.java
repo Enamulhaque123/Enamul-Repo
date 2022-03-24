5038f556aeb2cddef35c3e8a9878f7c90ce54d07
PC <PC@DESKTOP-LDUR2H9>
2022-03-24 14:45:35 +0530

    shakir modified

--- com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateContact.java
+++ com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateContact.java
@@ -4,6 +4,10 @@ public class CreateContact {
 
 	public static void main(String[] args) {
 		System.out.println("Create contact2");
+		
+		System.out.println("Shakir contact");
+		
+		
 	}
 
 }
--- com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateOrg1.java
+++ com.crm.vtiger2/src/test/java/com/crm/createOrg/CreateOrg1.java
@@ -4,6 +4,10 @@ public class CreateOrg1 {
 
 	public static void main(String[] args) {
 		System.out.println("Create Organization");
+		
+		System.out.println("shakir org");
+		
+		
 	}
 
 }
